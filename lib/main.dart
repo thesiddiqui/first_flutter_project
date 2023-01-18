b06809c0c9d177782fcc011455f3fa9655bcfd19
Nayem Siddiqui <62980753+thesiddiqui@users.noreply.github.com>
2023-01-18 12:06:00 +0600

    container and row

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
+      home: MyHome(),
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({Key? key}) : super(key: key);
-
-  MySanckbar(message, context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: message)
-    );
-  }
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My  Home"),
-        titleSpacing: 10,
-        toolbarHeight: 50,
-        toolbarOpacity: 1,
-        elevation: 7,
-        backgroundColor: Colors.black54,
+        title: Text('Home'),
         actions: [
-          IconButton(onPressed: () {MySanckbar(Text("I am Comment"), context);}, icon: Icon(Icons.comment)),
-          IconButton(onPressed: () {MySanckbar(Text("I am Search"), context);}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {MySanckbar(Text("I am Setting"), context);}, icon: Icon(Icons.settings)),
-         ],
+          IconButton(onPressed: () {}, icon: Icon(Icons.email)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){MySanckbar(Text("I am Floating Button"), context);},
-        elevation: 10,
+        onPressed: () {},
         child: Icon(Icons.home),
-        backgroundColor: Colors.black54,
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                  accountName: Text('Nayem Siddiqui'),
+                  accountEmail: Text('thesiddiqui@outlook.com'),
+                  currentAccountPicture: Image.network("https://support.appsflyer.com/hc/article_attachments/360004154837/flutter_icon.png"),
+              ),
+              
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.newspaper),
+              title: Text('News Feed'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logut'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.newspaper), label: 'News Feed'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+        ],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+
+          Container(height: 100, width: 100, child: Image.network('https://support.appsflyer.com/hc/article_attachments/360004154837/flutter_icon.png'),),
+          Container(height: 100, width: 100, child: Image.network('https://support.appsflyer.com/hc/article_attachments/360004154837/flutter_icon.png'),),
+          Container(height: 100, width: 100, child: Image.network('https://support.appsflyer.com/hc/article_attachments/360004154837/flutter_icon.png'),)
+        ],
+      )
     );
+    
   }
 }
